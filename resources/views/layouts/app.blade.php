35548bf309813f221d2ad848f9313d809699cee5
syuumu200 <syuumu200@gmail.com>
2020-08-31 22:38:50 +0900

    remove custom.css query tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
