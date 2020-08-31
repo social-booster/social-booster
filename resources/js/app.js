35b9a5c77d814e27a82103c0396ce08352da4b3c
syuumu200 <syuumu200@gmail.com>
2020-08-31 10:32:24 +0900

    force ssl

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,9 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+ if (process.env.MIX_APP_ENV !== 'local' && location.protocol === 'http') {
+   location.href = location.protocol + '//' + location.hostname + location.pathname
+ }
 require('./bootstrap');
 
 
