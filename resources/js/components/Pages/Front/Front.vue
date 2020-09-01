2c488ec69f61020021288e6103f71806c7903f4b
syuumu200 <syuumu200@gmail.com>
2020-09-01 11:13:13 +0900

    fix

--- resources/js/components/Pages/Front/Front.vue
+++ resources/js/components/Pages/Front/Front.vue
@@ -13,7 +13,7 @@
         </div>
         <b-row class="mt-4">
             <b-col xl="4" lg="4" md="4">
-                <h2 class="mt-5 mb-3 section-name"><strong>コンセプト</strong></h2>
+                <h2 class="mt-5 mb-3 section-name" style="border: none;"><strong>コンセプト</strong></h2>
                 <p>SBのコンセプトは”全体を貫く基本的な観点・考え方”という意味ではなく、SBで扱われるコンテンツの事を指します</p>
                 <router-link to="/document/concept" class="box-button">
                     <b-button variant="primary">
@@ -22,7 +22,7 @@
                 </router-link>
             </b-col>
             <b-col xl="4" lg="4" md="4">
-                <h2 class="mt-5 mb-3 section-name"><strong>紐付け</strong></h2>
+                <h2 class="mt-5 mb-3 section-name" style="border: none;"><strong>紐付け</strong></h2>
                 <p>コンセプト同士は網の目状に紐付けられています。紐付けによる再帰的な計算により優先順位の精度を向上させています</p>
                 <router-link to="/document/cover" class="box-button">
                     <b-button variant="primary">
@@ -31,7 +31,7 @@
                 </router-link>
             </b-col>
             <b-col xl="4" lg="4" md="4">
-                <h2 class="mt-5 mb-3 section-name"><strong>投票</strong></h2>
+                <h2 class="mt-5 mb-3 section-name" style="border: none;"><strong>投票</strong></h2>
                 <p>ユーザーはコンセプトと紐付けに対して投票を行う事ができます。この投票を元にSBは優先順位を算出します</p>
                 <router-link to="/document/vote" class="box-button">
                     <b-button variant="primary">
