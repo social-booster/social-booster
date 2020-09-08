0fbc6ccfc7fd3e787dd2eb12ecc42ef0ec5a48e4
syuumu200 <syuumu200@gmail.com>
2020-09-08 17:56:02 +0900

    code improvement -> Document

--- resources/js/components/Pages/Document/Document.vue
+++ resources/js/components/Pages/Document/Document.vue
@@ -3,46 +3,11 @@
     <h1>Document</h1>
     <b-row>
         <b-col class="text-left" xl="3" lg="3" md="3">
-            <router-link to="/document/outline" class="document-menu">
-                概略
-            </router-link>
-            <br>
-            <router-link to="/document/concept" class="document-menu">
-                コンセプト
-            </router-link>
-            <br>
-            <router-link to="/document/cover" class="document-menu">
-                紐付け
-            </router-link>
-            <br>
-            <router-link to="/document/vote" class="document-menu">
-                投票
-            </router-link>
-            <br>
-            <router-link to="/document/priority" class="document-menu">
-                優先順位の算出
-            </router-link>
-            <br>
-            <router-link to="/document/supporters" class="document-menu">
-                運営協力者の募集
-            </router-link>
-            <br>
-            <router-link to="/document/open-source" class="document-menu">
-                オープンソース
-            </router-link>
-            <br>
-            <router-link to="/document/notes" class="document-menu">
-                注意事項
-            </router-link>
-            <br>
-            <router-link to="/document/terms" class="document-menu">
-                利用規約
-            </router-link>
-            <br>
-            <router-link to="/document/privacy-policy" class="document-menu">
-                個人情報保護方針
-            </router-link>
-            <br>
+            <div v-for="docu in documents">
+                <router-link :to="'/document/' + docu.slug" class="document-menu">
+                    {{docu.title}}
+                </router-link>
+            </div>
         </b-col>
         <b-col xl="9" lg="9" md="9">
             <router-view />
@@ -53,6 +18,55 @@
 
 <script>
 export default {
+    data() {
+        return {
+            documents: [{
+                    slug: 'introduction',
+                    title: 'はじめに'
+                },
+                {
+                    slug: 'outline',
+                    title: '概略'
+                },
+                {
+                    slug: 'concept',
+                    title: 'コンセプト'
+                },
+                {
+                    slug: 'cover',
+                    title: '紐付け'
+                },
+                {
+                    slug: 'vote',
+                    title: '投票'
+                },
+                {
+                    slug: 'priority',
+                    title: '優先順位の算出'
+                },
+                {
+                    slug: 'supporters',
+                    title: '運営協力者の募集'
+                },
+                {
+                    slug: 'open-source',
+                    title: 'オープンソース'
+                },
+                {
+                    slug: 'notes',
+                    title: '注意事項'
+                },
+                {
+                    slug: 'terms',
+                    title: '利用規約'
+                },
+                {
+                    slug: 'privacy-policy',
+                    title: '個人情報保護方針'
+                }
+            ]
+        }
+    },
     mounted() {}
 }
 </script>
