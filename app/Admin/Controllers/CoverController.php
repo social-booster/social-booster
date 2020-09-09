978e4df7a3295c31b0fc1e62041ee25cb891ce05
syuumu200 <syuumu200@gmail.com>
2020-09-10 00:58:19 +0900

    add OrderBy desc -> CoverController -> Laravel-admin

--- app/Admin/Controllers/CoverController.php
+++ app/Admin/Controllers/CoverController.php
@@ -26,6 +26,7 @@ class CoverController extends AdminController
     {
         $grid = new Grid(new Cover());
 
+        $grid->model()->orderBy('created_at', 'desc');
         $grid->column('id', __('Id'));
         $grid->column('user_id', __('User id'))->hide();
         $grid->column('user.name');
