586659c28c8ac151e04571175c296163b63cc421
syuumu200 <syuumu200@gmail.com>
2020-09-10 00:59:32 +0900

    add OrderBy desc -> ConceptRealVoteController -> Laravel-admin

--- app/Admin/Controllers/ConceptRealVoteController.php
+++ app/Admin/Controllers/ConceptRealVoteController.php
@@ -26,6 +26,7 @@ class ConceptRealVoteController extends AdminController
     {
         $grid = new Grid(new ConceptRealVote());
 
+        $grid->model()->orderBy('created_at', 'desc');
         $grid->column('id', __('Id'));
         $grid->column('user_id', __('User id'))->hide();
         $grid->column('user.name');
