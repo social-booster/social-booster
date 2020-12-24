b6204d3b8342a796e491035c21b1e1cbfba44c0e
syuumu200 <syuumu200@gmail.com>
2020-12-25 07:15:39 +0900

    update sw-register.js

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,12 +1,14 @@
 if ('serviceWorker' in navigator) {
-  console.log('Service Worker');
-
-  navigator.serviceWorker.register('pwabuilder-sw.js')
-  .then(function (swReg) {
-    console.log('Service Worker is registered', swReg)
-    initialiseServiceWorker()
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error)
-  })
+    console.log('Service Worker');
+    navigator.serviceWorker.register('pwabuilder-sw.js')
+        .then(function(swReg) {
+            const ignore = ['/login', '/register', '/logout'];
+            if (ignore.includes(location.pathname)) {
+                swReg.unregister()
+            }
+            console.log('Service Worker is registered', swReg)
+        })
+        .catch(function(error) {
+            console.error('Service Worker Error', error)
+        })
 }
