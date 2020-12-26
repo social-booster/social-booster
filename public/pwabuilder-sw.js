3d5aef933d5e063a5590cb2dcf1bc93ff1080f4f
syuumu200 <syuumu200@gmail.com>
2020-12-27 03:23:41 +0900

    fix service worker

--- public/pwabuilder-sw.js
+++ public/pwabuilder-sw.js
@@ -4,15 +4,17 @@ const CACHE = "pwabuilder-precache";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
+
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+    if (event.data && event.data.type === "SKIP_WAITING") {
+        self.skipWaiting();
+    }
 });
+const reg=/^\/(document.*)?$/g
 
 workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
+    reg,
+    new workbox.strategies.CacheFirst({
     cacheName: CACHE
   })
 );
--- public/sw-register.js
+++ public/sw-register.js
@@ -2,10 +2,7 @@ if ('serviceWorker' in navigator) {
     console.log('Service Worker');
     navigator.serviceWorker.register('pwabuilder-sw.js')
         .then(function(swReg) {
-            const ignore = ['/login', '/register', '/logout'];
-            if (ignore.includes(location.pathname)) {
-                swReg.unregister()
-            }
+            swReg.update();
             console.log('Service Worker is registered', swReg)
         })
         .catch(function(error) {
