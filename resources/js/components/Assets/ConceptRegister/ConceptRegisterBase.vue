d3d5008dd075dc5723f24753d520d0b46110c7d8
syuumu200 <syuumu200pgmail.com>
2021-01-21 12:55:57 +0900

    Tone adjustment -> ConceptRegisterBase

--- resources/js/components/Assets/ConceptRegister/ConceptRegisterBase.vue
+++ resources/js/components/Assets/ConceptRegister/ConceptRegisterBase.vue
@@ -1,5 +1,5 @@
 <template>
-<div v-if="register.base !== null" class="mb-3" style="background: rgb(247, 247, 247) none repeat scroll 0% 0%; padding: 10px; border-radius: 10px;">
+<div v-if="register.base !== null" class="mb-3" style="background: rgb(250, 251, 251) none repeat scroll 0% 0%; padding: 10px; border-radius: 10px;">
     <p class="base-concept-name" v-if="register.base.name !== null">{{register.base.name}}</p>
     <p class="base-concept-content mb-0">{{register.base.content}}</p>
 </div>
