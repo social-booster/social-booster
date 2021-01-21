3c56ebb21406c41b8c9cb27d2b4a2d074a745cda
syuumu200 <syuumu200pgmail.com>
2021-01-21 14:18:35 +0900

    Make the menu follow the screen. -> Document

--- resources/js/components/Pages/Document/Document.vue
+++ resources/js/components/Pages/Document/Document.vue
@@ -3,10 +3,12 @@
     <h1>ドキュメント</h1>
     <b-row>
         <b-col class="text-left" xl="3" lg="3" md="3">
-            <div v-for="docu in documents">
-                <router-link :to="'/document/' + docu.slug" class="document-menu">
-                    {{docu.title}}
-                </router-link>
+            <div style="position: sticky;top: 0;">
+                <div v-for="docu in documents">
+                    <router-link :to="'/document/' + docu.slug" class="document-menu">
+                        {{docu.title}}
+                    </router-link>
+                </div>
             </div>
         </b-col>
         <b-col xl="9" lg="9" md="9">
