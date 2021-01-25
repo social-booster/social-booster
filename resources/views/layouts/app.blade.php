7c812f1851c2cf7081a0fe33bd9f883473494161
syuumu200 <syuumu200pgmail.com>
2021-01-25 11:29:53 +0900

    set favicon.ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
+    <!-- favicon -->
+    <link rel="shortcut icon" href="{{ asset('/favicon.ico') }}">
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
