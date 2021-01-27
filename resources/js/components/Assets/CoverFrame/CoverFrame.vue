9bae54ac7c5cb4839fcbb044bf5f56dbf8fb5b2c
syuumu200 <syuumu200pgmail.com>
2021-01-27 17:27:38 +0900

    code improvement -> CoverFrame

--- resources/js/components/Assets/CoverFrame/CoverFrame.vue
+++ resources/js/components/Assets/CoverFrame/CoverFrame.vue
@@ -1,19 +1,18 @@
 <template>
-<div>
-    <router-link :to="'/concept/' + concept.id" class="link">
-        <b-card class="mt-2" footer-class="pt-1 pb-1" header-class="">
-            <hr :class="'hr-' + concept.layer">
-            <span>{{concept.layer | layerName}}</span><span class="float-right">{{concept.user !== null ? concept.user.name : 'Anonymous'}}</span>
-            <hr>
-            <h2 v-show="concept.name !== null" class="cover-name">{{concept.name}}</h2>
-            <p>{{concept.content}}</p>
-            <div class="text-right">
-                <CoverFrameStats v-show="mode === 'upper'" :cover="cover" />
-                <CoverFrameVote v-show="mode === 'upper'" :cover="cover" />
-            </div>
-        </b-card>
-    </router-link>
-</div>
+<router-link :to="'/concept/' + concept.id" class="link">
+    <b-card tag="article" class="mt-2" footer-class="pt-1 pb-1" header-class="">
+        <hr :class="'hr-' + concept.layer">
+        <span>{{concept.layer | layerName}}</span>
+        <span class="float-right">{{concept.user !== null ? concept.user.name : 'Anonymous'}}</span>
+        <hr>
+        <h2 v-show="concept.name !== null" class="cover-name">{{concept.name}}</h2>
+        <p>{{concept.content}}</p>
+        <aside class="text-right">
+            <CoverFrameStats v-show="mode === 'upper'" :cover="cover" />
+            <CoverFrameVote v-show="mode === 'upper'" :cover="cover" />
+        </aside>
+    </b-card>
+</router-link>
 </template>
 
 <script>
