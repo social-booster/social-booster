29723667c9fefe2a45aeee3b129e41ff6e5e3f9d
syuumu200 <syuumu200pgmail.com>
2021-01-31 17:52:52 +0900

    code improvement -> Document

--- resources/js/components/Pages/Document/Document.vue
+++ resources/js/components/Pages/Document/Document.vue
@@ -2,7 +2,7 @@
 <div>
     <h1>ドキュメント</h1>
     <b-row>
-        <b-col class="text-left" xl="3" lg="3" md="3">
+        <b-col tag="aside" class="text-left" xl="3" lg="3" md="3">
             <nav style="position: sticky;top: 0;">
                 <ul class="p-0">
                     <li v-for="docu in docs" style="list-style: none;">
@@ -13,21 +13,19 @@
                 </ul>
             </nav>
         </b-col>
-        <b-col xl="9" lg="9" md="9">
-            <article class="mb-5">
-                <router-view />
-                <hr>
-                <nav v-if="true" :style="'column-count: ' + (doc_prev === undefined ? 1 : 2) + ';'">
-                    <div v-if="doc_prev !== undefined">
-                        <span>←</span>
-                        <router-link :to="'/document/' + doc_prev.slug">{{doc_prev.title}}</router-link>
-                    </div>
-                    <div v-if="doc_next !== undefined" class="text-right">
-                        <router-link :to="'/document/' + doc_next.slug">{{doc_next.title}}</router-link>
-                        <span>→</span>
-                    </div>
-                </nav>
-            </article>
+        <b-col tag="article" xl="9" lg="9" md="9" class="mb-5">
+            <router-view />
+            <hr>
+            <nav v-if="true" :style="'display: grid;grid-template-columns: ' + (doc_prev === undefined ? 'auto' : 'auto auto') + ';'">
+                <div v-if="doc_prev !== undefined">
+                    <b-icon icon="caret-left-fill"></b-icon>
+                    <router-link :to="'/document/' + doc_prev.slug">{{doc_prev.title}}</router-link>
+                </div>
+                <div v-if="doc_next !== undefined" class="text-right">
+                    <router-link :to="'/document/' + doc_next.slug">{{doc_next.title}}</router-link>
+                    <b-icon icon="caret-right-fill"></b-icon>
+                </div>
+            </nav>
         </b-col>
     </b-row>
 </div>
