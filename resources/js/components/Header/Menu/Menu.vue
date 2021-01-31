b74cab64cce9393353bf741fe0b92854f3e01d14
syuumu200 <syuumu200pgmail.com>
2021-01-31 17:54:51 +0900

    remove menu item -> Menu

--- resources/js/components/Header/Menu/Menu.vue
+++ resources/js/components/Header/Menu/Menu.vue
@@ -3,8 +3,6 @@
 <span v-if="is_login_loaded">
     <b-navbar-nav class="ml-auto" v-if="is_login">
         <b-nav-item-dropdown :text="my_user_data.name" right>
-            <b-dropdown-item href="/concepts/1">タイムライン</b-dropdown-item>
-            <b-dropdown-item href="/mypage">マイページ</b-dropdown-item>
             <b-dropdown-item @click="$store.dispatch('auth/logout')">ログアウト</b-dropdown-item>
         </b-nav-item-dropdown>
     </b-navbar-nav>
