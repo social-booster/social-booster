63c63462ef58d1a02373492331dee8b754e578d0
佐竹涼太 <satake00157@gmail.com>
2021-02-21 00:32:39 +0900

    laravelにコメントつけて、不要ファイルを削除した

--- docker/laravel/docker-entrypoint.sh
+++ docker/laravel/docker-entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# Laravelのcomposerによる最初のインストール
 composer self-update
 composer validate
 composer install
 
+# composer install後に生成させるファイルのファイル権限の変更
 chmod -R 777 ./bootstrap/cache
 chmod -R 777 ./storage
 
+# DBのマイグレート
 composer dump-autoload
 php artisan migrate
 
+# php-fpmをずっとフォアグラウンドで実行させる
 php-fpm
